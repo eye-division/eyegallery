55832a3b46831a7465241298c68746be30cb2f97
Neil C Smith <neil@eyedivision.com>
2016-01-11 16:46:34 +0000

    Move JavaScript out of HTML into separate file.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function($) {
+  "use strict"; // Start of use strict
+  $(document).ready(function() {
+
+    // Syntax highlighting
+    hljs.initHighlightingOnLoad();
+
+    // Header
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-navigation-menu').slideToggle(function(){
+        if($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
+        }
+      });
+    });
+  });
+
+})(jQuery); // End of use strict
