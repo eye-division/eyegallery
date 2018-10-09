ae316963c36bfa450d99db4e18ac81104526adbb
Alun Ward <a.ward@eyedivision.com>
2018-10-09 15:04:10 +0100

    Switch navigation menu to flexbox

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@
     //hljs.initHighlightingOnLoad();
 
     // Header
-    var menuToggle = $('#js-mobile-menu').unbind();
-    $('#js-navigation-menu').removeClass("show");
+    var menuToggle = $('#js--mobile-menu').unbind();
+    $('#js--navigation-menu').removeClass("show");
     menuToggle.on('click', function(e) {
       e.preventDefault();
-      $('#js-navigation-menu').slideToggle(function(){
-        if($('#js-navigation-menu').is(':hidden')) {
-          $('#js-navigation-menu').removeAttr('style');
+      $('#js--navigation-menu').slideToggle(function(){
+        if($('#js--navigation-menu').is(':hidden')) {
+          $('#js--navigation-menu').removeAttr('style');
         }
       });
     });
